5a1f703270785d3b9a762235a7b438aee4256997
Kevin M <kman17@gmail.com>
2017-03-25 12:44:05 -0400

    Added responsive flexbox properties to SWOT grid

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("begin").addEventListener("click", function(){
+  document.getElementById("header").className = "disappear";
+  document.getElementById("query").className = "disappear"
+  setTimeout(function(){
+    document.getElementById("header").className = "disappear offScreen";
+    document.getElementById("query").className = "appear";
+  }, 1200);
+})
+
+
+
+document.getElementById("start").addEventListener("click", function(){
+  var text = document.getElementById("theQuestion").value;
+  console.log(text);
+  document.getElementById("question").textContent = text;
+  document.getElementById("query").className = "disappear";
+  setTimeout(function(){
+    document.getElementById("query").style.transform = "translateX(1600px)";
+    document.getElementById("board").className = "moveup";
+  },1800);
+  setTimeout(function(){
+    document.getElementById("board").className = "appear moveup";
+  }, 2800)
+  return text;
+})
