964db9c6ca22a4fce44fc238331b07473579e878
Kevin M <kman17@gmail.com>
2017-03-25 16:02:37 -0400

    fixed scrollbars appearing for offscreen divs

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.getElementById("begin").addEventListener("click", function(){
   document.getElementById("header").className = "disappear";
   document.getElementById("query").className = "disappear"
+  document.getElementById("query").style.display = "block";
   setTimeout(function(){
     document.getElementById("header").className = "disappear offScreen";
+
     document.getElementById("query").className = "appear";
+
   }, 1200);
 })
 
@@ -13,8 +16,10 @@ document.getElementById("start").addEventListener("click", function(){
   var text = document.getElementById("theQuestion").value;
   console.log(text);
   document.getElementById("question").textContent = text;
+  document.getElementById("board").style.display = "block";
   document.getElementById("query").className = "disappear";
   setTimeout(function(){
+    document.getElementById("query").style.display = "none";
     document.getElementById("query").style.transform = "translateX(1600px)";
     document.getElementById("board").className = "moveup";
   },1800);
